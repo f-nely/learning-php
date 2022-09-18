482c46c1085344fcd83927f5e622716107c3a7bf
nely de faria <nelly.nf54@gmail.com>
2022-09-18 18:27:47 -0300

    Creating quotes.php

---
+++ learning-php/quotes.php
@@ -0,0 +1,25 @@
+<?php
+
+    /*
+        Double quotes
+        Strings in double quotes will be processed by PHP
+        before being outputted.
+    */
+
+    $name = 'Joe';
+    // echo "Hello, {$name}!";
+    // echo 'Welcome to Joe\'s Website.<br>';
+
+    /*
+        Escape characters
+        \n for a new line
+        \\ for a backslash
+        \$ for a dollar sign
+        \t for a tab
+    */
+
+    // echo "This is line 1 \n";
+    // echo "This is line 2";
+
+    // echo 'Joe\'s Nickname in HS was "Joey Calzone"';
+    echo "Joe's Nickname in HS was \"Joey Calzone\"";
