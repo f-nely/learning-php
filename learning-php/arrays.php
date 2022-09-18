d8d27621612bba7f6e122bfd68487150bec944a7
nely de faria <nelly.nf54@gmail.com>
2022-09-18 18:27:10 -0300

    Creating arrays.php

---
+++ learning-php/arrays.php
@@ -0,0 +1,30 @@
+<?php
+
+    // What are arrays and how do they work?
+
+    /*
+        Arrays
+        A collection of data that acts as key-value pairs
+        Arrays are great for complex data sets.
+
+        Types of arrays
+        - Indexable arrays (which use numbered keys).
+        - Associative arrays (which use strings as keys).
+
+        Important to know
+        Arrays index the start at 0, not 1.
+
+        Associative array
+        Allows you to turn the numbered index into a string.
+
+        Multidimensional arrays
+        An array of arrays.
+    */
+
+    // associative array
+    $home_towns = [
+        'Joe' => 'Middletown, NY',
+        'Erin' => 'West Chester, PA',
+        'Dave' => 'Exton, PA',
+        'Brian' => 'Grand Rapids, MI',
+    ];
