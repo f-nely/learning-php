4de8f8577275ef2d1f6071732512f06f88c7ee11
nely de faria <nelly.nf54@gmail.com>
2022-09-18 18:26:08 -0300

    Creating index.php

---
+++ learning-php/index.php
@@ -0,0 +1,14 @@
+<?php
+    // Single line comment: Printing on the screen
+    echo 'Hello World from echo.<br>';
+?>
+
+<?='Hello World from short tag.<br>'?>
+
+<?php
+    /*
+        We prefer to use echo.
+        print is a little slower.
+    */
+    print 'Hello World from print.';
+?>
