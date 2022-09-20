e13efbbdbfff76753a865273fc7ddc6eada27d92
nely de faria <nelly.nf54@gmail.com>
2022-09-20 20:39:03 -0300

    Create arithmetic-operators.php

---
+++ learning-php/arithmetic-operators.php
@@ -0,0 +1,23 @@
+<?php
+
+/* 
+    Arithmetic Operators
+    
+    Order of operations (PEMDAS)
+    1. Parentheses
+    2. Exponents
+    3. Multiplication / Division
+    4. Assition / Subtraction
+*/
+
+$a = 2;
+echo $a + 2; // This will output 4
+echo '<br>';
+
+echo 5 * 6 + 3 - 1; // This will output 32
+echo '<br>';
+
+echo 5 * (6 + 3) - 1; // This will output 44
+echo '<br>';
+
+echo 5 ** 2 * (6 + 3) - 1; // This will output 224
