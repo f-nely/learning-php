73ff0039836c221c25084440d8393a6b53f02114
nely de faria <nelly.nf54@gmail.com>
2022-09-23 16:36:22 -0300

    Create challenge-06.php

---
+++ learning-php/templates/challenge-06.php
@@ -0,0 +1,6 @@
+<?php include 'header.php'; ?>
+<!-- Main Content Start -->
+            <h2>Welcome!</h2>
+            <p>Hello world! This is my PHP website!</p>
+<!-- Main Content Start -->   
+<?php include 'footer.php'; ?>   
