bf6e620d1abb8cf1997467061ee13bd7174f97e4
nely de faria <nelly.nf54@gmail.com>
2022-09-23 16:37:35 -0300

    Create about.php

---
+++ learning-php/templates/about.php
@@ -0,0 +1,6 @@
+<?php include 'header.php'; ?>
+<!-- Main Content Start -->
+            <h2>About <?php echo $author; ?></h2>
+            <p>Biographical Text!</p>
+<!-- Main Content Start -->   
+<?php include 'footer.php'; ?>   
