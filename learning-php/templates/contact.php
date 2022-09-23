99a08432f1ce9437c9a61be081182a7e2e2d3ddb
nely de faria <nelly.nf54@gmail.com>
2022-09-23 16:37:52 -0300

    Create contact.php

---
+++ learning-php/templates/contact.php
@@ -0,0 +1,6 @@
+<?php include 'header.php'; ?>
+<!-- Main Content Start -->
+            <h2>Contact <?php echo $author; ?></h2>
+            <p>Email me joe [at] casabona.org</p>
+<!-- Main Content Start -->   
+<?php include 'footer.php'; ?>  
